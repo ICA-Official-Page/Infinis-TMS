03b58537f58666a7196fd08535b341169a693017
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2025-11-27 16:49:05 +0530

    our server url

--- frontend/src/utills.js
+++ frontend/src/utills.js
@@ -1,6 +1,6 @@
 
 // const URI = 'https://tms.infinis.io';
-const URI = 'http://localhost:8004'
+const URI = 'http://103.205.141.41:8004'
 // const URI = 'https://infinis-tms.onrender.com'
 
 export default URI;
